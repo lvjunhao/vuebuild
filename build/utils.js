2f6cf70825371e8bae9be2811cdfc6e8d825cf71
lvjunhao <lvjunhao1314@sina.com>
2019-03-14 16:55:04 +0800

    last

--- build/utils.js
+++ build/utils.js
@@ -28,16 +28,10 @@ exports.cssLoaders = function (options) {
       sourceMap: options.sourceMap
     }
   }
-  const px2remLoader = {
-    loader: 'px2rem-loader',
-    options: {
-      remUnit: 54
-    }
-  }
 
   // generate loader string to be used with extract text plugin
   function generateLoaders (loader, loaderOptions) {
-    const loaders = options.usePostCSS ? [cssLoader, postcssLoader, px2remLoader] : [cssLoader, px2remLoader]
+    const loaders = options.usePostCSS ? [cssLoader, postcssLoader] : [cssLoader]
 
     if (loader) {
       loaders.push({
--- src/main.js
+++ src/main.js
@@ -1,12 +1,9 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import '@styles/vueAnimate.css'
 import router from './router'
-import 'lib-flexible/flexible'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
-import '@styles/reset.css' // 包含部分充值Element-ui的样式,必须在element-ui css文件后引入
+import '@styles/reset.css' // 包含部分重置Element-ui的样式,必须在element-ui css文件后引入
 import * as datasmethod  from '@/filter/dataprocess/index.js'; // 数据加工过滤器
 import App from './App'
 import store from './store/index'
@@ -21,7 +18,7 @@ for (var key in datasmethod) {
 }
 new Vue({
   el: '#app',
-  router,
-  store,
+  router, // 注入路由
+  store, // 注入store(vuex)
   render: h => h(App)
 })
